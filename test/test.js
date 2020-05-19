4143f2fe90b00541feb598088d537d01bf493d6a
Andrey Mishchenko <msav@msav.ru>
2020-05-19 20:55:18 +0300

    Fix Windows test #2

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const fp = require('../index');
 
 describe('Windows: Filter Path', () => {
     it('Should return -1 when the target path is not equal.', () => {
-        const actual = fp('projects\\target', 'projects\\target\\assets');
+        const actual = fp('.\\projects\\target', '.\\projects\\target\\assets');
         const expected = './assets';
         assert.strictEqual(actual, expected);
     })
