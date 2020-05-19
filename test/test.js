7eeba6b8b3eb7082710027f04c3822f7a0977b4d
Andrey Mishchenko <msav@msav.ru>
2020-05-19 20:30:31 +0300

    Added tests

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const fp = require('../index');
+
+describe('Windows: Filter Path', () => {
+    it('Should return -1 when the target path is not equal.', () => {
+        const actual = fp('d:\\projects\\target', 'd:\\projects\\target\\assets');
+        const expected = './assets';
+        assert.strictEqual(actual, expected);
+    })
+});
+
+describe('Linux: Filter Path', () => {
+    it('Should return -1 when the target path is not equal.', () => {
+        const actual = fp('/home/user/projects/target', '/home/user/projects/target/assets');
+        const expected = './assets';
+        assert.strictEqual(actual, expected);
+    })
+});
