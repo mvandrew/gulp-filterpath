9782c7222851d0fba0f9942463b68dcfc1f5b495
Andrey Mishchenko <msav@msav.ru>
2020-05-19 21:06:11 +0300

    Windows and Linux using

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require("path");
 
 module.exports = (root_path, target_path) => {
-    return "./" + path.relative(root_path, target_path).replace(/\\/g, '/');
+    return "./" + path.relative(root_path.replace(/\\/g, '/'), target_path.replace(/\\/g, '/')).replace(/\\/g, '/');
 };
